eae986660cec134cf09aa28f13d847ccbd3878f5
Laurent <laurent.roches.simplon@gmail.com>
2024-02-27 12:45:19 +0100

    Petite mise en page et page formateur

--- tableau-admin.php
+++ tableau-admin.php
@@ -28,57 +28,59 @@ $reservations = $BDD->getAllReservations();
     <link rel="stylesheet" href="./style.css">
 </head>
 <body>
-    <div>
-        <table>
-            <caption><h1>Liste des utilisateurs</h1></caption>
-            <tr>
-                <th>ID</th>
-                <th>NOM</th>
-                <th>Prenom</th>
-                <th>email</th>
-                <th>rôle</th>
-            </tr>
-            <?php foreach ($utilisateurs as $utilisateur) { ?>
-            <tr>
-                <td><?= $utilisateur->getId() ?></td>
-                <td><?= $utilisateur->getNom() ?></td>
-                <td><?= $utilisateur->getPrenom() ?></td>
-                <td><?= $utilisateur->getMail() ?></td>
-                <td><?= $utilisateur->getRole() ?></td>
-            </tr>
-            <?php } ?>
-        </table>
-    </div>
-    <div>
-        <table>
-            <caption><h1>Liste des réservations</h1></caption>
-            <tr>
-                <th>ID</th>
-                <th>Nombre de places</th>
-                <th>Pass</th>
-                <th>Journées</th>
-                <th>Nuits</th>
-                <th>Enfants</th>
-                <th>Casques</th>
-                <th>Luges</th>
-                <th>Total</th>
-                <th>email</th>
-            </tr>
-            <?php foreach ($reservations as $reservation) { ?>
-            <tr>
-                <td><?= $reservation->getId() ?></td>
-                <td><?= $reservation->getnombrePlaces() ?></td>
-                <td><?= $reservation->getpass() ?></td>
-                <td><?= $reservation->getjournee() ?></td>
-                <td><?= $reservation->getnuit() ?></td>
-                <td><?= $reservation->getenfants() ?></td>
-                <td><?= $reservation->getcasques() ?></td>
-                <td><?= $reservation->getluges() ?></td>
-                <td><?= $reservation->gettotal() ?></td>
-                <td><?= $reservation->getMail() ?></td>
-            </tr>
-            <?php } ?>
-        </table>
-    </div>
+    <fieldset>
+        <div>
+            <table>
+                <caption><h1>Liste des utilisateurs</h1></caption>
+                <tr>
+                    <th>ID</th>
+                    <th>NOM</th>
+                    <th>Prenom</th>
+                    <th>email</th>
+                    <th>rôle</th>
+                </tr>
+                <?php foreach ($utilisateurs as $utilisateur) { ?>
+                <tr>
+                    <td><?= $utilisateur->getId() ?></td>
+                    <td><?= $utilisateur->getNom() ?></td>
+                    <td><?= $utilisateur->getPrenom() ?></td>
+                    <td><?= $utilisateur->getMail() ?></td>
+                    <td><?= $utilisateur->getRole() ?></td>
+                </tr>
+                <?php } ?>
+            </table>
+        </div>
+        <div>
+            <table>
+                <caption><h1>Liste des réservations</h1></caption>
+                <tr>
+                    <th>ID</th>
+                    <th>Nombre de places</th>
+                    <th>Pass</th>
+                    <th>Journées</th>
+                    <th>Nuits</th>
+                    <th>Enfants</th>
+                    <th>Casques</th>
+                    <th>Luges</th>
+                    <th>Total</th>
+                    <th>email</th>
+                </tr>
+                <?php foreach ($reservations as $reservation) { ?>
+                <tr>
+                    <td><?= $reservation->getId() ?></td>
+                    <td><?= $reservation->getnombrePlaces() ?></td>
+                    <td><?= $reservation->getpass() ?></td>
+                    <td><?= $reservation->getjournee() ?></td>
+                    <td><?= $reservation->getnuit() ?></td>
+                    <td><?= $reservation->getenfants() ?></td>
+                    <td><?= $reservation->getcasques() ?></td>
+                    <td><?= $reservation->getluges() ?></td>
+                    <td><?= $reservation->gettotal() ?></td>
+                    <td><?= $reservation->getMail() ?></td>
+                </tr>
+                <?php } ?>
+            </table>
+        </div>
+    </fieldset>
 </body>
 </html>
